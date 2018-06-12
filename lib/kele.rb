c1bc329e52a4b91f7874efbc53224dbcd2ce3e5b
Todd Carlson <tcarlson@carlsonsw.com>
2018-06-12 19:58:45 -0400

    mentor check

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,6 +13,14 @@ class Kele
   def get_me
     response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
     @user_data = JSON.parse(response.body)
+    #get mentor_id = user_data[mentor_id]
+  end
+
+  def get_mentor_availability(mentor_id)
+    #Get JSON array
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    #Convert the JSON response to a Ruby array
+    @mentor_availability = JSON.parse(response.body)
   end
 
   private
